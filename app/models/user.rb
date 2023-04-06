ca5c4d61b5f65d16dbac6f4316c564d6863ed86b
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-04-06 15:18:04 +0000

    [Add] ユーザーマイページ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :posts, dependent: :destroy
-  
+
   has_one_attached :profile_image
-  
+
   # enum設定
   enum sex: { men:0, women:1, others:2 }
-  
+
   validates :name, presence: true
   validates :email, presence: true
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/icon-user.png')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jp', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/icon_user.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
-    profile_image.variant(resize_to_lomit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
