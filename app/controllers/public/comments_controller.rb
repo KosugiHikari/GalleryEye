90632cd7d966f531d7cadc9c889656f5312fa3dc
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-04-10 06:32:34 +0000

    [Update] コメント機能非同期化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,22 +1,23 @@
 class Public::CommentsController < ApplicationController
-  
+
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.save
+    flash.now[:notice] = 'コメントを投稿しました'
   end
-  
+
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    flash.now[:notice] = 'コメントを削除しました'
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:comment)
   end
-  
+
 end
