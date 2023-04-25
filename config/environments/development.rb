0e3774e2e80ebd251a3953c29365559d8c28b976
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-04-25 09:20:14 +0000

    [Add] パスワードリセットメール機能追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { protocol: 'https' , host: 'c9565dcfa5d748feb3dede3f07d05158.vfs.cloud9.ap-northeast-1.amazonaws.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+   }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
